6f8cc1af0e5fa403f519c03173042dfc3a626223
kazyuaki <kazuyama195@gmail.com>
2025-07-02 11:11:15 +0900

    出品商品情報登録 テスト実装

--- src/tests/Feature/user/UserProfileEditTest.php
+++ src/tests/Feature/user/UserProfileEditTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,31 @@ class UserProfileEditTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
+
+    public function testProfileEditFormShowsPreviousValues()
+    {
+        /** @var \App\Models\User $user */
+
+        $user = User::factory()->create([
+            'name' => 'テスト太郎',
+            'post_code' => '123-4567',
+            'address' => '大阪市中央区',
+            'building_name' => 'ビルディング101',
+            'profile_image' => 'profiles/test.jpg',
+        ]);
+
+        $this->actingAs($user);
+
+        $response = $this->get(route('profile.edit'));
+
+        $response->assertOk();
+        $response->assertSee('テスト太郎');
+        $response->assertSee('123-4567');
+        $response->assertSee('大阪市中央区');
+        $response->assertSee('ビルディング101');
+        $response->assertSee('profiles/test.jpg');
     }
+
 }
+
